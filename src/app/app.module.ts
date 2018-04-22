f63dc8ef94ecabbf880bc56c74ae6be060f20b54
Kateryna Purchel <kateryna@ardanis.com>
2018-04-22 22:17:15 +0100

    country search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -10,6 +11,7 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ApiProvider } from '../providers/api/api';
+import { AutoCompleteModule } from 'ionic2-auto-complete';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { ApiProvider } from '../providers/api/api';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    HttpClientModule
+    HttpClientModule,
+    HttpModule,
+    AutoCompleteModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,15 +1,30 @@
+import {AutoCompleteService} from 'ionic2-auto-complete';
 import { HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map'
 
 @Injectable()
-export class ApiProvider {
+export class ApiProvider implements AutoCompleteService {
+  labelAttribute = "name";
 
-  constructor(public http: HttpClient) {
+  constructor(
+    public httpClient: HttpClient,
+    private http:Http) {
     console.log('Hello ApiProvider Provider');
   }
 
   getWeather() {
-    // return this.http.get('https://swapi.co/api/films');
-    return this.http.get('http://api.openweathermap.org/data/2.5/weather?id=7778677&APPID=ec44e7ff2e0597cf35d85dd13062e22d');
+    return this.httpClient.get('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=ec44e7ff2e0597cf35d85dd13062e22d');
+  }
+
+  getResults(keyword:string) {
+    return this.http.get("https://restcountries.eu/rest/v1/name/"+keyword)
+      .map(
+        result =>
+        {
+          return result.json()
+            .filter(item => item.name.toLowerCase().startsWith(keyword.toLowerCase()) )
+        });
   }
 }
