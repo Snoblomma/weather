04a316eb670250e482a99edaa9e8b6545ae878f3
Kate Kate <snoblomma@gmail.com>
2018-06-25 23:05:02 +0100

    test categories lsit

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { SettingsProvider } from './../../providers/settings/settings';
 
 @IonicPage()
 @Component({
@@ -7,12 +8,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'categories.html',
 })
 export class CategoriesPage {
+  public selectedTheme: string;
+  public categories: any = [
+    { icon: "bulb", name: "This is bulb" }, 
+    { icon: "cog", name: "This is cog" }];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private viewCtrl: ViewController,
+    private settings: SettingsProvider) {
+    this.initialize();
   }
 
   ionViewDidLoad() {
+  }
 
+  initialize() {
+    this.settings.getActiveTheme().subscribe(
+      val => {
+        if (val == "dark-theme") {
+          this.selectedTheme = "dark-theme";
+        }
+        else {
+          this.selectedTheme = "light-theme";
+        }
+      });
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
